86c46baf3719ef4e28be885567484d7ba12f1d69
Vorfall <alekseevegorc@gmail.com>
2021-06-21 16:25:44 +0300

    dateformat

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,21 @@
 package subtask1
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        var newStr:String = ""
+
+        SimpleDateFormat("yyyy-M-d", Locale("ru")).parse("$year-$month-$day")
+        if ( SimpleDateFormat("d").format(SimpleDateFormat("yyyy-M-d", Locale("ru")).parse("$year-$month-$day")) == day && SimpleDateFormat("M").format(
+                SimpleDateFormat("yyyy-M-d", Locale("ru")).parse("$year-$month-$day")) == month && SimpleDateFormat("yyyy").format(
+                SimpleDateFormat("yyyy-M-d", Locale("ru")).parse("$year-$month-$day")) == year )
+            newStr = SimpleDateFormat("dd MMMM, EEEE", Locale("ru")).format(SimpleDateFormat("yyyy-M-d", Locale("ru")).parse("$year-$month-$day"))
+        else
+            newStr = "Такого дня не существует"
+        return newStr
     }
 }
