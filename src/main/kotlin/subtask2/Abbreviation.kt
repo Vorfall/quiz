b1dd1d3261646c719c1cab39015313755acbf040
Vorfall <alekseevegorc@gmail.com>
2021-06-21 16:22:31 +0300

    add solution abreviation

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,10 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        return b.contentEqualsYesNo(a)
+    }
+    private fun String.contentEqualsYesNo(s: String): String{
+        return if (this.contentEquals(s.toUpperCase().filter { this.contains(it) })) "YES" else "NO"
     }
 }
